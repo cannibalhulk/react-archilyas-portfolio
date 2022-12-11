f8382f2a7f9461435d7000599d1590473c54f2e8
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2022-12-12 00:39:40 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './components/Home'
+// import Navbar from './components/Navbar'
+// import Footer from './components/Footer'
+import Projects from './components/Projects'
+import ProjectSingle from './components/ProjectSingle'
+import Contact from './components/Contact'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Home/>} exact /> 
+        <Route  path="/layiheler" element={<Projects/>}/> 
+        <Route  path="/layiheler/:slug" element={<ProjectSingle/>}/> 
+        <Route  path="/elaqe" element={<Contact/>}/> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
