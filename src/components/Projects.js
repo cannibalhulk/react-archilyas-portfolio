e6388d3b4d57d8b15417af3bf2f4a8ed8a1f281e
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-02-01 14:30:59 +0400

    project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,40 +1,44 @@
 import { Carousel } from "@mantine/carousel";
+import { MantineProvider } from "@mantine/core";
 import Footer from "./Footer";
 import "../Projects.css";
 
 export default function Project() {
   return (
-    <div>
-      <Carousel
-        sx={{ maxWidth: 920 }}
-        mx="auto"
-        withIndicators
-        height={"auto"}
-        width={"auto"}
-        styles={{
-          indicator: {
-            width: 12,
-            height: 4,
-            transition: "width 250ms ease",
-
-            "&[data-active]": {
-              width: 70,
+    <MantineProvider>
+      <div>
+        <Carousel
+          sx={{ maxWidth: 1080, marginTop:22 }}
+          mx="auto"
+          radius="sm"
+          withIndicators
+          height={"auto"}
+          width={"auto"}
+          styles={{
+            indicator: {
+              width: 12,
+              height: 4,
+              transition: "width 250ms ease",
+              
+              "&[data-active]": {
+                width: 70,
+              },
             },
-          },
-        }}
-      >
-        <Carousel.Slide className="p">
-          <img src={require("../images/villa-1/hazir-2.jpg")} alt="img" />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <img src={require("../images/villa-1/hazir3.jpg")} alt="img" />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <img src={require("../images/villa-1/hazir-render2.jpg")} alt="img" />
-        </Carousel.Slide>
-        {/* ...other slides */}
-      </Carousel>
-      <Footer/>
-    </div>
+          }}
+          >
+          <Carousel.Slide className="p">
+            <img src={require("../images/villa-1/hazir-2.jpg")} alt="img" />
+          </Carousel.Slide>
+          <Carousel.Slide>
+            <img src={require("../images/villa-1/hazir3.jpg")} alt="img" />
+          </Carousel.Slide>
+          <Carousel.Slide>
+            <img src={require("../images/villa-1/hazir-render2.jpg")} alt="img" />
+          </Carousel.Slide>
+          {/* ...other slides */}
+        </Carousel>
+        <Footer/>
+      </div>
+    </ MantineProvider>
   );
 }
