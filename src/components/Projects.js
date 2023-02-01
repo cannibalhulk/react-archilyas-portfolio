0f0ab7d28b4d2100368351d5c18a867a7a955857
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-02-01 15:45:25 +0400

    add navbar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,69 @@
 import { Carousel } from "@mantine/carousel";
 import { MantineProvider } from "@mantine/core";
+import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import "../Projects.css";
 
 export default function Project() {
   return (
     <MantineProvider>
+      <nav id="navbar" className="navbar-b">
+        <div
+          className="navbar-overlay"
+          onClick={() => {
+            const toggleMenuOpen = () => document.body.classList.toggle("open");
+            toggleMenuOpen();
+          }}
+        ></div>
+
+        <button
+          type="button"
+          className="navbar-burger"
+          onClick={() => {
+            const toggleMenuOpen = () => document.body.classList.toggle("open");
+            toggleMenuOpen();
+          }}
+        >
+          <span className="material-icons">menu</span>
+        </button>
+        <Link to="/">
+          <a>
+            <img src={require("../archilyas-logo.png")} alt="logo" />
+          </a>
+        </Link>
+
+        <nav className="navbar-menu">
+          <Link to="/">
+            <a>
+              <button type="button" className="active">
+                Ana Səhifə
+              </button>
+            </a>
+          </Link>
+          <Link to="/#haqqimda">
+            <a href="#haqqimda">
+              <button type="button">Haqqımda</button>
+            </a>
+          </Link>
+          <Link to="/layiheler">
+            <a>
+              <button type="button">Layihələr</button>
+            </a>
+          </Link>
+          <Link to="/blog">
+            <a>
+              <button type="button">Blog</button>
+            </a>
+          </Link>
+          <Link to="/elaqe">
+            <a>
+              <button className="contact-me" type="button">
+                Əlaqə
+              </button>
+            </a>
+          </Link>
+        </nav>
+      </nav>
       <div className="">
         <Carousel
           sx={{ maxWidth: 1080, marginTop:22 }}
